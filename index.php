f4edd8768a9fd107638d4024911f653e8f6a75c5
Rikelme <cicerorikelmelopesbatista@gmail.com>
2022-05-04 10:21:25 -0300

    Index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+   
+</head>
+<body>
+<header>
+    <div class="um">
+    <div class="dois">
+         <h1>TK LANCHES</h1>
+   </div>
+<div class="tres">
+   <ul>
+     <li><a href="">bolos </a> </li>
+     <li><a href="">criador</a>  </li>
+     <li><a href="">cadastre se</li>
+  </ul>
+</div>
+  </div>
+ </header>
+</body>
+</html>
